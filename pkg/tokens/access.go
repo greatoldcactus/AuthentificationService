cda6ba2d95cf9e0603333cb9e546d2bb07a7510a
greatoldcactus <greatoldcactus@noreply.com>
2024-09-08 00:16:53 +0300

    Commented tokens packages

--- pkg/tokens/access.go
+++ pkg/tokens/access.go
@@ -2,21 +2,26 @@ package tokens
 
 import "time"
 
+// AccessToken is JWT token
 type AccessToken struct {
 	Signature string             `json:"signature"`
 	Payload   AccessTokenPayload `json:"payload"`
 	Header    AccessTokenHeader  `json:"header"`
 }
 
+// AccessTokenPayload is payload for AccessToken
+// Currently empty
 type AccessTokenPayload struct {
 }
 
+// Header for AccessToken
 type AccessTokenHeader struct {
 	Type string    `json:"typ"`
 	Alg  string    `json:"alg"`
 	Exp  time.Time `json:"exp"`
 }
 
+// NewAccessToken creates a new AccessToken with a specified expiration time.
 func NewAccessToken(Exp time.Time) AccessToken {
 	return AccessToken{
 		Header: AccessTokenHeader{
--- pkg/tokens/refresh.go
+++ pkg/tokens/refresh.go
@@ -2,14 +2,18 @@ package tokens
 
 import "time"
 
+// RefreshToken is used to Refresh Access token
 type RefreshToken struct {
 	Payload RefreshTokenPayload `json:"payload"`
 }
 
+// RefreshTokenPayload is payload for Refresh token
+// Contains signature of AccessToken to be used with
 type RefreshTokenPayload struct {
 	AccessTokenSignature string `json:"accTokenSignature"`
 }
 
+// RefreshTokenHeader is header for Refresh token
 type RefreshTokenHeader struct {
 	Expires time.Time `json:"exp"`
 }
