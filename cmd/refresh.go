37f6bd721af1adecb00f93c52140177b69e155e6
greatoldcactus <greatoldcactus@noreply.com>
2024-09-08 13:58:58 +0300

    Add refresh route

---
+++ cmd/refresh.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	api "authservice/pkg/api"
+	"authservice/pkg/auth"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func handleRefresh(w http.ResponseWriter, r *http.Request) {
+
+	if err := validateAuthRequest(w, r); err != nil {
+		return
+	}
+
+	// GUID := r.Header.Get("Guid")
+	// addr := r.RemoteAddr
+
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		log.Default().Printf("failed to read all data from request body: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var p api.RefreshAccessTokenPair
+
+	err = json.Unmarshal(body, &p)
+
+	if err != nil {
+		log.Default().Printf("failed to unmarshall RefreshAccessTokenPair from request: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("there is no Refresh Access token pair in request"))
+		return
+	}
+
+	refreshToken, err := api.LoadRefreshTokenFromBase64(p.RefreshToken)
+
+	if err != nil {
+		log.Default().Printf("failed to load Refresh token from request: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("there is incorrect Refresh token in request"))
+		return
+	}
+
+	// TODO add check for Refresh token signature
+
+	accessToken := p.AccessToken
+
+	accessTokenSignature, err := auth.CalculateAccessTokenHash(accessToken, secret)
+
+	if err != nil {
+		log.Default().Printf("failed to calculate Access token signature: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if accessToken.Signature != accessTokenSignature {
+		log.Default().Printf("attempted to refresh with access token with incorrect signature: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("there is incorrect Refresh token in request"))
+		return
+	}
+
+	if refreshToken.Payload.AccessTokenSignature != accessTokenSignature {
+		log.Default().Printf("attempted to refresh with signature in refresh token not equal to signature of access token: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("there is incorrect Refresh token in request"))
+		return
+	}
+
+	// TODO invalidate old Refresh token in DB
+
+	tokenPair, err := generateAccessRefreshPair()
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Default().Printf("error when trying to generate Refresh Access token pair: %v", err)
+		return
+	}
+
+	answerJson, err := json.Marshal(tokenPair)
+
+	if err != nil {
+		log.Default().Println("auth answer json marshalling error error: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(answerJson)
+
+}
---
+++ cmd/refresh_test.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	api "authservice/pkg/api"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestRefreshOk(t *testing.T) {
+	recorder := httptest.NewRecorder()
+
+	tokens, err := generateAccessRefreshPair()
+
+	requestBody, err := json.Marshal(tokens)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	request, err := http.NewRequest(http.MethodPost, "/v1/auth", bytes.NewBuffer(requestBody))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	request.Header.Add("Guid", "hello")
+
+	handler := http.HandlerFunc(handleAuth)
+
+	handler.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+
+	if response.StatusCode != 200 {
+		t.Fatalf("Request failed with code: %v", response.Status)
+	}
+
+	responseBody, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		t.Fatalf("Failed to read response body")
+	}
+
+	var tokensResult api.RefreshAccessTokenPair
+
+	err = json.Unmarshal(responseBody, &tokensResult)
+
+	if err != nil {
+		t.Fatalf("failed to unmarshall answer from server: %v", err)
+	}
+
+	fmt.Printf("%#v", tokensResult)
+
+}
+
+func TestRefreshNoGuid(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest(http.MethodPost, "/v1/auth", strings.NewReader(""))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler := http.HandlerFunc(handleAuth)
+
+	handler.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+
+	if response.StatusCode == 200 {
+		t.Fatalf("Passed wrong request with no GUID")
+	}
+}
+
+func TestRefreshTooMuchGuid(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest(http.MethodPost, "/v1/auth", strings.NewReader(""))
+
+	request.Header.Add("Guid", "hello")
+	request.Header.Add("Guid", "there")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler := http.HandlerFunc(handleAuth)
+
+	handler.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+
+	if response.StatusCode == 200 {
+		t.Fatalf("Passed wrong request with too much GUID")
+	}
+}
+
+func TestRefreshInvalidMethod(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest(http.MethodGet, "/v1/auth", strings.NewReader(""))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler := http.HandlerFunc(handleAuth)
+
+	handler.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+
+	if response.StatusCode == 200 {
+		t.Fatalf("Passed wrong request with incorrect method")
+	}
+}
