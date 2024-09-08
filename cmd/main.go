9ed77182c0e00cc8369bec3ae4e278d27667c628
greatoldcactus <greatoldcactus@noreply.com>
2024-09-08 15:27:14 +0300

    Added secret loading from environment

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"authservice/pkg/mail"
+	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
-// TODO add loading from environment?
-var secret string = "my secret"
+var secret string
 
+const MinSecretLength int = 16
 const AccessTokenDuration time.Duration = time.Hour * 2
 const RefreshTokenDuration time.Duration = time.Hour * 24 * 30
 
 var mailer mail.Mailer = mail.SimpleMailer{}
 
 func main() {
+	secret = os.Getenv("SECRET")
+
+	if len(secret) < MinSecretLength {
+		msg := fmt.Sprintf("Secret is too short: %v, expected at least: %v bytes", len(secret), MinSecretLength)
+		panic(msg)
+	}
+
 	http.HandleFunc("/v1/auth", handleAuth)
 
 	http.HandleFunc("/v1/refresh", handleRefresh)
