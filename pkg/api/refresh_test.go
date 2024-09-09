84ea79ab3a312e2f53de0fce2eb788f81f0386f6
greatoldcactus <greatoldcactus@noreply.com>
2024-09-09 10:53:16 +0300

    add test for token hash changed after loading

--- pkg/api/refresh_test.go
+++ pkg/api/refresh_test.go
@@ -1,6 +1,7 @@
 package tokens
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -44,6 +45,43 @@ func TestLoadRefreshTokenFromBase64(t *testing.T) {
 
 	if tokenLoaded.Payload != token.Payload {
 		t.Errorf("loaded token payload is not equal to initial token:\n%#v\n%#v", tokenLoaded.Payload, token.Payload)
+	}
+}
+
+func TestRefreshTokenHashChangeAfterLoad(t *testing.T) {
+
+	token := NewRefreshToken("hello", time.Now(), "your ip")
+
+	tokenBase64, err := token.Base64()
+
+	if err != nil {
+		t.Errorf("failed token base64 function")
+	}
+
+	tokenLoaded, err := LoadRefreshTokenFromBase64(tokenBase64)
+
+	if err != nil {
+		t.Errorf("failed to load token from base64 encoded string: %v", err)
+	}
+
+	guid := "g"
+
+	hash1, err := token.Hash(guid)
+
+	if err != nil {
+		t.Errorf("failed to calculate token hash: %v", err)
+	}
+
+	hash2, err := tokenLoaded.Hash(guid)
+
+	if err != nil {
+		t.Errorf("failed to calculate token hash: %v", err)
+	}
+
+	if hash1 != hash2 {
+		fmt.Printf("hashes: \n%v\n%v\n", hash1, hash2)
+		fmt.Printf("tokens: \n%#v\n%#v\n", token, tokenLoaded)
+		t.Errorf("token hash was changed after saving and loading!")
 	}
 }
 
